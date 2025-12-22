abdbd07331a9028aee6c5eb000a677ce8049318f
Kpek142 <kirillkrec412@gmail.com>
2025-12-22 23:48:16 +0700

    CitySearch

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.weatherapp {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
+    requires org.json;
+    requires java.net.http;
 
     opens com.example.weatherapp to javafx.fxml;
     exports com.example.weatherapp;
